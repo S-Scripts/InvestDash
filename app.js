ed99e7a8b880b058aae63ca8ea298c3f4b0fe3ea
S-Scripts <shreyahpadmanabha@gmail.com>
2024-10-31 13:49:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const chart = document.querySelector("#chart").getContext('2d');
+
+// Create new chart instance
+new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
+        datasets: [
+            {
+                label: 'BTC',
+                data: [29374, 33537, 49631, 59095, 57828, 36684, 33572, 39974, 48847, 48116, 61004],
+                borderColor: 'red',
+                borderWidth: 2
+            },
+            {
+                label: 'ETH',
+                data: [31500, 33347, 38631, 79055, 37828, 36688, 69572, 39004, 49947, 46000, 36890],
+                borderColor: 'blue',
+                borderWidth: 2
+            }
+        ]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+// Show or hide sidebar
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+});
+
+// Change theme
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+});
